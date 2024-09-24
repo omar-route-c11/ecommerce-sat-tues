f3b13b1fe42eabf1fb47838e076351f7d428eb44
Omar Assem <omarassem.route6@gmail.com>
2024-09-24 22:40:57 +0300

    Implement get categories

--- lib/features/home/presentation/widgets/category_item.dart
+++ lib/features/home/presentation/widgets/category_item.dart
@@ -1,11 +1,14 @@
-import 'package:ecommerce/core/resources/assets_manager.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ecommerce/core/resources/color_manager.dart';
 import 'package:ecommerce/core/resources/styles_manager.dart';
+import 'package:ecommerce/features/home/domain/entities/category.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryItem extends StatelessWidget {
-  const CategoryItem();
+  const CategoryItem(this.category);
+
+  final Category category;
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +22,15 @@ class CategoryItem extends StatelessWidget {
             decoration: const BoxDecoration(
               shape: BoxShape.circle,
             ),
-            child: Image.asset(
-              ImageAssets.categoryHomeImage,
-              fit: BoxFit.cover,
+            child: CachedNetworkImage(
+              imageUrl: category.imageURL,
+              fit: BoxFit.fill,
             ),
           ),
         ),
         SizedBox(height: 8.h),
         Text(
-          "men's fashion",
+          category.name,
           style: getRegularStyle(color: ColorManager.darkBlue, fontSize: 14.sp),
         ),
       ],
--- lib/features/home/presentation/widgets/home_tab.dart
+++ lib/features/home/presentation/widgets/home_tab.dart
@@ -1,10 +1,16 @@
 import 'dart:async';
 
+import 'package:ecommerce/core/di/service_locator.dart';
 import 'package:ecommerce/core/resources/assets_manager.dart';
+import 'package:ecommerce/core/widgets/error_indicator.dart';
+import 'package:ecommerce/core/widgets/loading_indicator.dart';
+import 'package:ecommerce/features/home/presentation/cubit/home_cubit.dart';
+import 'package:ecommerce/features/home/presentation/cubit/home_states.dart';
 import 'package:ecommerce/features/home/presentation/widgets/announcements_section.dart';
 import 'package:ecommerce/features/home/presentation/widgets/category_item.dart';
 import 'package:ecommerce/features/home/presentation/widgets/custom_section_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeTab extends StatefulWidget {
@@ -22,11 +28,13 @@ class _HomeTabState extends State<HomeTab> {
     ImageAssets.carouselSlider2,
     ImageAssets.carouselSlider3,
   ];
+  late final HomeCubit _homeCubit;
 
   @override
   void initState() {
     super.initState();
     _startImageSwitching();
+    _homeCubit = serviceLocator.get<HomeCubit>();
   }
 
   @override
@@ -47,12 +55,29 @@ class _HomeTabState extends State<HomeTab> {
               ),
               SizedBox(
                 height: 270.h,
-                child: GridView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (_, __) => const CategoryItem(),
-                  itemCount: 20,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
+                child: BlocProvider(
+                  create: (_) => _homeCubit,
+                  child: BlocBuilder<HomeCubit, HomeState>(
+                    builder: (context, state) {
+                      if (state is GetCategoriesLoading) {
+                        return const LoadingIndicator();
+                      } else if (state is GetCategoriesError) {
+                        return ErrorIndicator(state.message);
+                      } else if (state is GetCategoriesSuccess) {
+                        return GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          itemBuilder: (_, index) =>
+                              CategoryItem(state.categories[index]),
+                          itemCount: state.categories.length,
+                          scrollDirection: Axis.horizontal,
+                        );
+                      } else {
+                        return const SizedBox();
+                      }
+                    },
                   ),
                 ),
               ),
